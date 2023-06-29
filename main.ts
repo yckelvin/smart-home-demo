b0f0795253a77834e3eeb9d2c546984ad622c6fb
Kelvin Lee <yckelvin@users.noreply.github.com>
2023-06-29 11:27:18 +0800

    correct the logic

--- main.ts
+++ main.ts
@@ -9,10 +9,12 @@ microIoT.microIoT_showUserText(0, "INIT DEVICE")
 microIoT.microIoT_clear()
 basic.showNumber(2)
 microIoT.microIoT_showUserText(0, "Ready!")
+microIoT.microIoT_ServoRun(microIoT.aServos.S1, 0)
+microIoT.microIoT_ServoRun(microIoT.aServos.S2, 90)
 basic.forever(function () {
     light2 = pins.analogReadPin(AnalogPin.P0)
     microIoT.microIoT_showUserText(1, "Light: " + convertToText(light2))
-    if (light2 < 200) {
+    if (light2 > 300) {
         pins.analogWritePin(AnalogPin.P1, 1023)
     } else {
         pins.analogWritePin(AnalogPin.P1, 0)
