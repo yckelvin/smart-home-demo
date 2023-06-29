3acad4ab437c43998fc0c5bca113294117774f5c
Kelvin Lee <yckelvin@users.noreply.github.com>
2023-06-29 11:17:25 +0800

    change p16 to p1 and remove rainfall

--- main.ts
+++ main.ts
@@ -1,38 +1,22 @@
 input.onButtonPressed(Button.AB, function () {
     control.reset()
 })
-let rainfall = 0
 let light2 = 0
-let wifi_name = "izowifi"
-let password = "izo1234@"
+basic.showNumber(0)
 microIoT.microIoT_initDisplay()
+basic.showNumber(1)
 microIoT.microIoT_showUserText(0, "INIT DEVICE")
-microIoT.microIoT_showUserText(1, "SETUP WIFI")
-microIoT.microIoT_WIFI(wifi_name, password)
 microIoT.microIoT_clear()
+basic.showNumber(2)
 microIoT.microIoT_showUserText(0, "Ready!")
+microIoT.microIoT_ServoRun(microIoT.aServos.S1, 0)
+microIoT.microIoT_ServoRun(microIoT.aServos.S2, 90)
 basic.forever(function () {
     light2 = pins.analogReadPin(AnalogPin.P0)
     microIoT.microIoT_showUserText(1, "Light: " + convertToText(light2))
-    if (light2 < 128) {
-        pins.analogWritePin(AnalogPin.P16, 1023)
+    if (light2 < 200) {
+        pins.analogWritePin(AnalogPin.P1, 1023)
     } else {
-        pins.analogWritePin(AnalogPin.P16, 0)
+        pins.analogWritePin(AnalogPin.P1, 0)
     }
-    rainfall = pins.analogReadPin(AnalogPin.P1)
-    microIoT.microIoT_showUserText(2, "Rainfall: " + convertToText(rainfall))
-    if (rainfall > 400) {
-        microIoT.microIoT_ServoRun(microIoT.aServos.S1, 30)
-        basic.showLeds(`
-            # # # # #
-            # . # . #
-            . . . . .
-            # . # . #
-            . . . . .
-            `)
-    } else {
-        microIoT.microIoT_ServoRun(microIoT.aServos.S1, 0)
-        basic.clearScreen()
-    }
-    basic.pause(5000)
 })
